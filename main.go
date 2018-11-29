9da975e7355995b404297338995968a77a46b696
nico <brun.nicolas21@gmail.com>
2018-11-29 15:35:57 +0100

    cleaning from previous version

--- main.go
+++ main.go
@@ -9,16 +9,26 @@ import (
 	"io"
 	"log"
 	"os"
-	"path/filepath"
+	// "path/filepath"
 	"sort"
 	"strings"
 )
 
 var FCC = []byte("MMA ")
 
+const (
+	MilFlag  = 0xFFFE
+	FileFlag = 0xFFFF
+	LineSize = 64
+)
+
+type mvis struct {
+	File    string
+	Count   int
+	Missing int
+}
+
 func main() {
-	file := flag.String("f", "", "file")
-	erase := flag.Bool("x", false, "erase")
 	flag.Parse()
 	ps := flag.Args()
 	if len(ps) == 0 {
@@ -30,53 +40,51 @@ func main() {
 			log.Fatalln(err)
 		}
 	}
-	if *erase && *file != "" {
-		err := os.Remove(*file)
-		if err != nil && !os.IsNotExist(err) {
-			log.Fatalln(err)
-		}
-	}
-	dir := filepath.Dir(*file)
-	if err := os.MkdirAll(dir, 0755); dir != "" && err != nil && !os.IsExist(err) {
+	rs, err := NewReader(ps)
+	if err != nil {
 		log.Fatalln(err)
 	}
-	var w io.Writer
-	switch f, err := os.OpenFile(*file, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644); {
-	case *file == "":
-		w = os.Stdout
-	case *file != "" && err == nil:
-		defer f.Close()
-		w = f
-	default:
-		log.Fatalln(err)
-	}
-	sort.Strings(ps)
-	for _, p := range ps {
-		if strings.HasSuffix(p, ".bad") {
-			continue
+	r := bufio.NewReader(rs)
+	for {
+		body := make([]byte, LineSize)
+		for {
+			if _, err := io.ReadFull(r, body); err != nil {
+				log.Fatalln(err)
+			}
+			sequence := binary.BigEndian.Uint16(body)
+			if sequence == MilFlag {
+				continue
+			} else if sequence == FileFlag {
+				size := binary.BigEndian.Uint32(body[2:])
+				name := bytes.Trim(body[6:], "\x00")
+				log.Printf("%d: %s (%s)", size, name, rs.Filename())
+				break
+			} else {
+				log.Fatalf("invalid sequence %d", sequence)
+			}
 		}
-		if err := process(w, p); err != nil {
-			log.Printf("%s: %s", p, err)
+		for {
+			if bs, err := r.Peek(2); err == nil {
+				s := binary.BigEndian.Uint16(bs)
+				if s == MilFlag || s == FileFlag {
+					break
+				}
+			}
+			if _, err := io.ReadFull(r, body); err != nil {
+				log.Fatalln(err)
+			}
+			sequence := binary.BigEndian.Uint16(body)
+			log.Printf("%4d (%04[1]x): %x", sequence, body[2:])
 		}
 	}
-}
-
-func cleanPaths(ps []string) []string {
-	sort.Strings(ps)
-	for _, p := range ps {
-		if strings.HasSuffix(p, ".bad") {
-			continue
-		}
-	}
-	return ps
 }
 
 type fileReader struct {
-	ps []string
+	ps   []string
 	file *os.File
 }
 
-func NewReader(ps []string) (io.Reader, error) {
+func NewReader(ps []string) (*fileReader, error) {
 	sort.Strings(ps)
 	var xs []string
 	for _, p := range ps {
@@ -101,6 +109,10 @@ func NewReader(ps []string) (io.Reader, error) {
 	return &fileReader{file: f, ps: xs}, nil
 }
 
+func (f *fileReader) Filename() string {
+	return f.file.Name()
+}
+
 func (f *fileReader) Read(bs []byte) (int, error) {
 	if len(f.ps) == 0 && f.file == nil {
 		return 0, io.EOF
@@ -110,12 +122,13 @@ func (f *fileReader) Read(bs []byte) (int, error) {
 		if len(f.ps) > 0 {
 			f.file.Close()
 			f.file, err = openFile(f.ps[0])
+			log.Println("openFile", f.ps[0])
 			if len(f.ps) == 1 {
 				f.ps = f.ps[:0]
-				} else {
-					f.ps = f.ps[1:]
-				}
-				return f.Read(bs)
+			} else {
+				f.ps = f.ps[1:]
+			}
+			return f.Read(bs)
 		} else {
 			f.file = nil
 		}
@@ -141,60 +154,54 @@ func openFile(f string) (*os.File, error) {
 	return r, err
 }
 
-const (
-	MilFlag  = 0xFFFE
-	FileFlag = 0xFFFF
-	LineSize = 64
-)
-
-func process(ws io.Writer, p string) error {
-	r, err := os.Open(p)
-	if err != nil {
-		return err
-	}
-	defer r.Close()
-	magic := make([]byte, 4)
-	if _, err := r.Read(magic); err != nil {
-		return err
-	}
-	if !bytes.Equal(magic, FCC) {
-		return fmt.Errorf("expected magic %s (found: %s)", FCC, magic)
-	}
-	if _, err := r.Seek(12, io.SeekCurrent); err != nil {
-		return err
-	}
-	log.Println()
-	var w bytes.Buffer
-	for {
-		bs := make([]byte, LineSize)
-		if _, err := io.ReadFull(r, bs); err != nil {
-			if err == io.EOF {
-				break
-			}
-			return err
-		}
-		sequence := binary.BigEndian.Uint16(bs)
-		switch sequence {
-		case MilFlag:
-			continue
-		case FileFlag:
-			size := binary.BigEndian.Uint32(bs[2:])
-			name := bytes.Trim(bs[6:], "\x00")
-			log.Printf("%d: %s", size, name)
-		default:
-		}
-		log.Printf("%4d: %x", binary.BigEndian.Uint16(bs), bs)
-
-		bs = bytes.Trim(bs[2:], "\x00")
-		for i := 0; i < len(bs); i += 2 {
-			w.WriteByte(bs[i])
-			if j := i + 1; j < len(bs) {
-				w.WriteByte(bs[j])
-			}
-		}
-	}
-	if w.Len() > 0 {
-		io.WriteString(ws, w.String())
-	}
-	return nil
-}
+// func process(ws io.Writer, p string) error {
+// 	r, err := os.Open(p)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer r.Close()
+// 	magic := make([]byte, 4)
+// 	if _, err := r.Read(magic); err != nil {
+// 		return err
+// 	}
+// 	if !bytes.Equal(magic, FCC) {
+// 		return fmt.Errorf("expected magic %s (found: %s)", FCC, magic)
+// 	}
+// 	if _, err := r.Seek(12, io.SeekCurrent); err != nil {
+// 		return err
+// 	}
+// 	log.Println()
+// 	var w bytes.Buffer
+// 	for {
+// 		bs := make([]byte, LineSize)
+// 		if _, err := io.ReadFull(r, bs); err != nil {
+// 			if err == io.EOF {
+// 				break
+// 			}
+// 			return err
+// 		}
+// 		sequence := binary.BigEndian.Uint16(bs)
+// 		switch sequence {
+// 		case MilFlag:
+// 			continue
+// 		case FileFlag:
+// 			size := binary.BigEndian.Uint32(bs[2:])
+// 			name := bytes.Trim(bs[6:], "\x00")
+// 			log.Printf("%d: %s", size, name)
+// 		default:
+// 		}
+// 		log.Printf("%4d: %x", binary.BigEndian.Uint16(bs), bs)
+//
+// 		bs = bytes.Trim(bs[2:], "\x00")
+// 		for i := 0; i < len(bs); i += 2 {
+// 			w.WriteByte(bs[i])
+// 			if j := i + 1; j < len(bs) {
+// 				w.WriteByte(bs[j])
+// 			}
+// 		}
+// 	}
+// 	if w.Len() > 0 {
+// 		io.WriteString(ws, w.String())
+// 	}
+// 	return nil
+// }
